f4cea6963765f4b1fc76b9a443793973aff23077
mariusz-dabal <mariusz9dabal@gmail.com>
2020-04-19 14:30:22 +0200

    Add auth views

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+        width: {
+            '96': '24rem'
+        },
+        colors: {
+            'france-blue': '#2F80ED',
+            'gunmetal' : '#2D3047',
+            'lava': '#BB0A21',
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
